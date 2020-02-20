4474519a10cc8fe80b1678362ebe0f041450ce0c
DELL <waigolotimothy@gmail.com>
2020-02-20 15:57:09 +0300

    my second app edition

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mySong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySong = MediaPlayer.create(MainActivity.this,R.raw.yesu);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySong.pause();
     }
 }
